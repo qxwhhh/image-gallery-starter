78f8e080a169c95300daf334bb375ad518817717
Jockiery <liaozhixin666@gmail.com>
2024-04-16 19:04:34 +0800

    Update Logo.tsx

--- components/Icons/Logo.tsx
+++ components/Icons/Logo.tsx
@@ -1,5 +1,6 @@
 export default function Logo() {
   return (
-   < img alt="avatar" loading="lazy" width="80" height="80" decoding="async" data-nimg="1" class="rounded-full z-10" style="color:transparent" srcset="https://res-console.cloudinary.com/dqmfeq3rk/media_explorer_thumbnails/9e248145b6151639343fc1284b3f7c90/detailed" />
-  )
+< img alt="avatar" loading="lazy" width="80" height="80" decoding="async"
+data-nimg="1" class="rounded-full z-10" style="color:transparent" 
+src="https://file.notion.so/f/f/c731fc92-f643-4cac-bc7d-79e9430089b7/23f979fb-c4e0-469a-b4f5-905bd686ee63/1000.webp?id=f72a148e-4a9f-4b0e-9965-f6a0391920b2&table=block&spaceId=c731fc92-f643-4cac-bc7d-79e9430089b7&expirationTimestamp=1713355200000&signature=zzXvjsayg7iECWAJRy5sU915I71ID2nEkIk0_Js9yUo&downloadName=1000.webp">  )
 }
