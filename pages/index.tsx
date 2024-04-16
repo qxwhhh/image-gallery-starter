e52a01a8777e0d396a612df14a7f3ea698fa461e
Jockiery <liaozhixin666@gmail.com>
2024-04-16 19:53:32 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,8 +65,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
               Use the action of pressing the shutter to freeze memories and use the lens to discover the beauty of the world.
             </p>
-            <a class="cursor-pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4" href="https://twitter.com/Jockiery" target="_blank" rel="noreferrer">Follow me</a >          
-          </div>
+            <a className="cursor-pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4" href="https://twitter.com/Jockiery" target="_blank" rel="noreferrer">Follow me</a >          </div>
           
           {images.map(({ id, public_id, format, blurDataUrl }) => (
             <Link
