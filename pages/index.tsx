4abb41b404707459fb632d106571fe727ff724eb
Jockiery <liaozhixin666@gmail.com>
2024-04-16 18:50:36 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,6 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
             </div>
             
-             <img alt="avatar" loading="lazy" width="80" height="80" decoding="async" data-nimg="1" class="rounded-full z-10" style="color:transparent" src="https://res-console.cloudinary.com/dqmfeq3rk/media_explorer_thumbnails/9e248145b6151639343fc1284b3f7c90/detailed" />         
             
             <Logo />
             
