68e22b88f816912f425c1fa9aec82994dc9d602b
qxwhhh <159096946+qxwhhh@users.noreply.github.com>
2024-04-16 20:40:14 +0800

    Update Logo.tsx

--- components/Icons/Logo.tsx
+++ components/Icons/Logo.tsx
@@ -8,7 +8,7 @@ export default function Logo() {
       decoding="async"
       data-nimg="1" 
       className="rounded-full z-10" 
-      src="https://file.notion.so/f/f/c731fc92-f643-4cac-bc7d-79e9430089b7/23f979fb-c4e0-469a-b4f5-905bd686ee63/1000.webp?id=f72a148e-4a9f-4b0e-9965-f6a0391920b2&table=block&spaceId=c731fc92-f643-4cac-bc7d-79e9430089b7&expirationTimestamp=1713355200000&signature=zzXvjsayg7iECWAJRy5sU915I71ID2nEkIk0_Js9yUo&downloadName=1000.webp"
+      src="https://file.notion.so/f/f/9616ac00-f2b2-4626-9ca7-ce69e43a8803/06c0d95e-8a23-494a-8ac2-8f129c469afc/tumblr_19d005b9e0889a5bdbc4e64987d409c0_f47ef57e_540.jpeg?id=5ad69362-45b2-40f4-bcf0-8b9b0de5ea0a&table=block&spaceId=9616ac00-f2b2-4626-9ca7-ce69e43a8803&expirationTimestamp=1713362400000&signature=bvoiPNa6rkufwduyUP2PzERV2yJ0BrMuFIwrSz5hXII&downloadName=tumblr_19d005b9e0889a5bdbc4e64987d409c0_f47ef57e_540.jpeg"
     />
   );
 }
