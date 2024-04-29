7c7f1f3b02064cc079361c8b5eeecae318f4546c
qxwhhh <159096946+qxwhhh@users.noreply.github.com>
2024-04-29 21:02:51 +0800

    Update Logo.tsx

--- components/Icons/Logo.tsx
+++ components/Icons/Logo.tsx
@@ -1,15 +1,14 @@
-import logoImage from 'components/Icons/logo.jpeg';
 export default function Logo() {
   return (
     <img 
-      alt="avatar" 
+      alt="Estelle" 
       loading="lazy" 
       width="100" 
       height="100" 
       decoding="async"
       data-nimg="1" 
       className="rounded-full z-10" 
-      src={logoImage}
+      src=“https://64.media.tumblr.com/46d991c971243ae45971545870d8c476/c353c37b1876639d-0b/s1280x1920/abf3df9fe45014d3928959cb885f4757e19acf6f.pnj”
     />
   );
 }
