dec2d8b221ea002b21902e525f907d3f3e46b670
qxwhhh <159096946+qxwhhh@users.noreply.github.com>
2024-04-29 21:40:05 +0800

    Update Logo.tsx

--- components/Icons/Logo.tsx
+++ components/Icons/Logo.tsx
@@ -1,27 +1,10 @@
 export default function Logo() {
   return (
-    <svg
-      aria-labelledby="conf-logo-title-header"
-      fill="none"
-      role="img"
-      viewBox="0 0 200 200"
-      width="200"
-      height="200"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <title id="conf-logo-title-header">Estelle</title>
-      <path
-        d="M100 180 C 50 180 50 20 50 20 C 50 180 100 180 100 180 Z"
-        fill="currentColor"
-      ></path>
-      <path
-        d="M100 100 C 50 100 50 50 50 50 C 50 100 100 100 100 100 Z"
-        fill="currentColor"
-      ></path>
-      <path
-        d="M100 0 C 50 0 50 0 50 50 C 50 100 100 100 100 100 Z"
-        fill="currentColor"
-      ></path>
-    </svg>
+<svg aria-labelledby="conf-logo-title-header" fill="none" role="img" viewBox="0 0 200 200" width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+  <title id="conf-logo-title-header">Estelle</title>
+  <path d="M100 180 C 50 180 50 20 50 20 C 50 180 100 180 100 180 Z" fill="currentColor"></path>
+  <path d="M100 100 C 50 100 50 50 50 50 C 50 100 100 100 100 100 Z" fill="currentColor"></path>
+  <path d="M100 0 C 50 0 50 0 50 50 C 50 100 100 100 100 100 Z" fill="currentColor"></path>
+</svg>
   );
 }
