c67a44f7dbdaea17223b9d55780bd6fac6c0a96c
qxwhhh <159096946+qxwhhh@users.noreply.github.com>
2024-05-11 21:23:53 +0800

    Update Logo.tsx

--- components/Icons/Logo.tsx
+++ components/Icons/Logo.tsx
@@ -1,10 +1,14 @@
 export default function Logo() {
   return (
-<svg aria-labelledby="conf-logo-title-header" fill="none" role="img" viewBox="0 0 200 200" width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-  <title id="conf-logo-title-header">Estelle</title>
-  <path d="M100 180 C 50 180 50 20 50 20 C 50 180 100 180 100 180 Z" fill="currentColor"></path>
-  <path d="M100 100 C 50 100 50 50 50 50 C 50 100 100 100 100 100 Z" fill="currentColor"></path>
-  <path d="M100 0 C 50 0 50 0 50 50 C 50 100 100 100 100 100 Z" fill="currentColor"></path>
-</svg>
+    < img 
+      alt="avatar" 
+      loading="lazy" 
+      width="100" 
+      height="100" 
+      decoding="async"
+      data-nimg="1" 
+      className="rounded-full z-10" 
+      src="https://raw.githubusercontent.com/estelledc/photos/master/20240509232836.png"
+    />
   );
 }
